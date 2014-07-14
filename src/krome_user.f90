9171f8a86709cfa94a191abd8465c1a88d92fc5d
Troels Haugboelle <haugboel@nbi.dk>
2014-07-14 16:14:42 +0200

    ADD krome_get_mu_x function.

--- src/krome_user.f90
+++ src/krome_user.f90
@@ -376,6 +376,18 @@ contains
   end function krome_get_coef
 
   !****************************
+  !get the mean molecular weight from
+  ! mass fractions
+  function krome_get_mu_x(xin)
+    use krome_commons
+    implicit none
+    real*8::krome_get_mu_x,xin(:)
+    real*8::n(nmols)
+    n(:) = krome_x2n(xin(:),1d0)
+    krome_get_mu_x = krome_get_mu(n(:))
+  end function krome_get_mu_x
+
+  !****************************
   !return the adiabatic index from mass fractions
   ! and temperature in K
   function krome_get_gamma_x(xin,inTgas)
