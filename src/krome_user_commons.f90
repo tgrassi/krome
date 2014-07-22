b8d3984df91f8d58fa23489b60af2b77a53f70b8
zlaptoppeo <zlaptoppeo@zlaptop.(none)>
2014-07-22 18:55:06 +0200

    added modified. reason: see previous

---
+++ src/krome_user_commons.f90
@@ -0,0 +1,27 @@
+module krome_user_commons
+  implicit none
+
+#KROME_header
+  
+  !user can add here the common variables needed
+  !for rate calculation (e.g. optical depth, CR rate, 
+  !pressure, density, ...)
+  real*8::tau,zrate,pah_size,gas_dust_ratio,krome_redshift
+  real*8::krome_J21,krome_J21xr,user_tff
+  real*8::krome_invdvdz !inverse of |velocity gradient| 1/abs(dv/dz)
+
+  !$omp threadprivate(user_tff,krome_invdvdz)
+
+contains 
+
+  !user can add here the functions he/she needs for
+  !rate calculations (Kooij funcion provided as example)
+  function kooij(kalpha,kbeta,kgamma,Tgas)
+    real*8::kooij,kalpha,kbeta,kgamma,Tgas
+    kooij = kalpha*(Tgas/3d2)**kbeta*exp(-kgamma/Tgas)
+  end function kooij
+
+
+end module krome_user_commons
+
+
