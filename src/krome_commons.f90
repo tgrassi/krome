551f3f84e03d61cf0a5665d2764d0356a4d17cfa
tgrassi <tommasograssi@gmail.com>
2015-04-20 14:47:09 +0200

    logical for opt table loading

--- src/krome_commons.f90
+++ src/krome_commons.f90
@@ -42,6 +42,7 @@ module krome_commons
   real*8,allocatable::dust_Qabs(:,:),dust_Qabs_E(:)
   real*8,allocatable::dust_intBB(:,:),dust_intBB_sigma(:,:)
   real*8,allocatable::dust_intBB_dT(:,:)
+  logical::Qabs_allocated(ndustTypes)
   real*8::xdust(ndust)
   integer::krome_dust_partner_idx(ndustTypes),dust_Qabs_nE
   integer,parameter::dust_nT=int(1e5)
