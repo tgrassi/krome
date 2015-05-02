edaaae40dff53c8db2a232b68032ab7a079dc27c
tgrassi <tommasograssi@gmail.com>
2015-05-02 13:41:52 +0200

    tabs rearrange

--- src/krome_dust.f90
+++ src/krome_dust.f90
@@ -805,12 +805,18 @@ contains
     use krome_subs
     implicit none
 
-    call init_anytab2D("dust_table_cool.dat",dust_tab_ngas(:), dust_tab_Tgas(:), &
-         dust_tab_cool(:,:), dust_mult_ngas, dust_mult_Tgas)
-    call init_anytab2D("dust_table_Tdust.dat",dust_tab_ngas(:), dust_tab_Tgas(:), &
-         dust_tab_Tdust(:,:), dust_mult_ngas, dust_mult_Tgas)
-    call init_anytab2D("dust_table_H2.dat",dust_tab_ngas(:), dust_tab_Tgas(:), &
-         dust_tab_H2(:,:), dust_mult_ngas, dust_mult_Tgas)
+    call init_anytab2D("dust_table_cool.dat",dust_tab_ngas(:),&
+         dust_tab_Tgas(:), dust_tab_cool(:,:), dust_mult_ngas, &
+         dust_mult_Tgas)
+    call init_anytab2D("dust_table_heat.dat",dust_tab_ngas(:),&
+         dust_tab_Tgas(:), dust_tab_heat(:,:), dust_mult_ngas, &
+         dust_mult_Tgas)
+    call init_anytab2D("dust_table_Tdust.dat",dust_tab_ngas(:), &
+         dust_tab_Tgas(:), dust_tab_Tdust(:,:), dust_mult_ngas, &
+         dust_mult_Tgas)
+    call init_anytab2D("dust_table_H2.dat",dust_tab_ngas(:), &
+         dust_tab_Tgas(:), dust_tab_H2(:,:), dust_mult_ngas, &
+         dust_mult_Tgas)
 
   end subroutine init_dust_tabs
 
