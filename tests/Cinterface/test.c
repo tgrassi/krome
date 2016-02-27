10ed92ffa609ed952fc4839fc9745ceddcb51081
Jon Ramsey <jon.p.ramsey@gmail.com>
2016-02-27 21:33:36 +0100

    DEV Added `Cinterface` test to `krome/tests`.
    - Set up with `./krome -test=Cinterface`.
    
    - Compile in `build/` and then run `./kromeC`, `./kromeF`, and finally
      `python check_diffs.py` to check for differences in the output files.
      They should be identical.

---
+++ tests/Cinterface/test.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+
+#include "krome.h"
+#include "krome_user.h"
+
+int main() {
+  double Tgas, dt, spy, t, ntot, x_60Fe, Fe60_xi;
+  double x[krome_nmols];
+  int nstep, i;
+  FILE * fp;
+
+  spy = krome_seconds_per_year; // seconds per year
+
+  krome_init();
+
+  /* ----==== Isotope decay off ====---- */
+  for(i=0;i < krome_nmols;i++) {
+    x[i] = 1e-20; // default abundances
+  }
+  ntot = 1.0e4; // cm^-3
+  x[krome_idx_Hj] = ntot; // H+ initial abundance
+
+  // set iron abundance
+  krome_scale_z(x, 0.0);
+  x[krome_idx_FEj] = x[krome_idx_FE];
+  x[krome_idx_FE] = 0.0;
+  x[krome_idx_E] = krome_get_electrons(x);
+
+  // set 60Fe abundance
+  krome_set_user_tauh(1.5e6 * spy);
+  x_60Fe = 0.0;
+  printf("Fe-60 abundance = %E\n", x_60Fe);
+  x[krome_idx_60FE] = x_60Fe * x[krome_idx_FEj];
+  // set rate for ionisation by isotope decay
+  Fe60_xi = 0.0;
+  printf("Isotope decay off.\n");
+  krome_set_user_xi(Fe60_xi);
+  // set heating from isotope decay
+  krome_set_user_wergs(36.0 * krome_eV_to_erg);
+
+  Tgas = 1.0e3;
+
+  t = 0.0;
+  dt = 1.0e-2 * spy;
+  nstep = 0;
+  fp = fopen("idoff.c.dat","w");
+  if (fp == NULL) {
+    printf("Cannot open outputfile!\n");
+    return 1;
+  }
+  while (t <= 1.0e8 * spy) {
+    dt = dt * 1.1;
+    t = t + dt;
+    if (nstep % 10 == 0) {
+      printf("nstep = %d\n",nstep);
+    }
+
+    // call KROME
+    krome(x, &Tgas, &dt);
+    nstep++;
+
+    // output results to file
+    fprintf(fp, "%15.8E%15.8E", t/spy, Tgas);
+    for (i = 0;i < krome_nmols;i++){
+      fprintf(fp, "%15.8E", x[i]/ntot);
+    }
+    fprintf(fp, "\n");
+
+    krome_popcool_dump(t/spy, 71);
+  }
+
+  printf("Finished. Number of steps = %d\n", nstep);
+
+  /* ----==== Isotope decay on ====---- */
+  for(i=0;i < krome_nmols;i++) {
+    x[i] = 1e-20; // default abundances
+  }
+  ntot = 1.0e4; // cm^-3
+  x[krome_idx_Hj] = ntot; // H+ initial abundance
+
+  // set iron abundance
+  krome_scale_z(x, 0.0);
+  x[krome_idx_FEj] = x[krome_idx_FE];
+  x[krome_idx_FE] = 0.0;
+  x[krome_idx_E] = krome_get_electrons(x);
+
+  // set 60Fe abundance
+  krome_set_user_tauh(1.5e6 * spy);
+  x_60Fe = 1.0e-6;
+  printf("Fe-60 abundance = %E\n", x_60Fe);
+  x[krome_idx_60FE] = x_60Fe * x[krome_idx_FEj];
+  // set rate for ionisation by isotope decay
+  Fe60_xi = 1.0e-10;
+  printf("Isotope decay on.\n");
+  krome_set_user_xi(Fe60_xi);
+  // set heating from isotope decay
+  krome_set_user_wergs(36.0 * krome_eV_to_erg);
+
+  Tgas = 1.0e3;
+
+  t = 0.0;
+  dt = 1.0e-2 * spy;
+  nstep = 0;
+  fp = fopen("idon.c.dat","w");
+  if (fp == NULL) {
+    printf("Cannot open outputfile!\n");
+    return 1;
+  }
+  while (t <= 1.0e8 * spy) {
+    dt = dt * 1.1;
+    t = t + dt;
+    if (nstep % 10 == 0) {
+      printf("nstep = %d\n",nstep);
+    }
+
+    // call KROME
+    krome(x, &Tgas, &dt);
+    nstep++;
+
+    // output results to file
+    fprintf(fp, "%15.8E%15.8E", t/spy, Tgas);
+    for (i = 0;i < krome_nmols;i++){
+      fprintf(fp, "%15.8E", x[i]/ntot);
+    }
+    fprintf(fp, "\n");
+
+    krome_popcool_dump(t/spy, 72);
+  }
+  fclose(fp);
+
+  printf("Finished. Number of steps = %d\n", nstep);
+
+  return 0;
+}
---
+++ tests/Cinterface/test.f90
@@ -0,0 +1,109 @@
+program test
+  use krome_main !use krome (mandatory)
+  use krome_user !use utility (for krome_idx_* constants and others)
+  implicit none
+  integer:: nstep
+  integer, parameter:: ioin = 10
+  integer, parameter:: nsp=krome_nmols !number of species (common)
+  real*8:: Tgas, dt, x(nsp), spy, t, ntot, eVperg, fe60_xi, x_Fe60
+  
+  spy = krome_seconds_per_year !seconds per year
+  
+  call krome_init() !init krome (mandatory)
+
+  ! ----==== Isotope decay off ====---- !
+  
+  x(:) = 1d-20 !default abundances
+  ntot = 1.0d4 ! cm**-3
+  x(krome_idx_Hj) = ntot !hydrogen ion initial abundance
+
+  ! set iron abundance
+  call krome_scale_Z(x(:),0.0d0)
+  x(krome_idx_Fej) = x(krome_idx_Fe)
+  x(krome_idx_Fe) = 0.0d0
+  x(krome_idx_E) = krome_get_electrons(x(:))
+
+  ! set 60Fe abundance
+  call krome_set_user_tauh(1.5d6 * spy)
+  x_Fe60 = 0.0d0
+  print*, 'Fe-60 abundance =',x_Fe60
+  x(krome_idx_60Fe) = x_Fe60 * x(krome_idx_Fej)
+  ! set rate for ionisation by isotope decay
+  fe60_xi = 0.0d0
+  print*, 'Isotope decay off.'
+  call krome_set_user_xi(fe60_xi)
+  ! set heating from isotope decay
+  call krome_set_user_Wergs(36.0d0 * krome_eV_to_erg)
+
+  Tgas = 1.0d3 !gas temperature (K)
+
+  open (66, file='idoff.f.dat', status='unknown', form='formatted')
+
+  t = 0.0d0
+  dt = 1d-2 * spy ! yr -> s
+  nstep = 0
+  do
+    dt = dt * 1.1d0
+    t = t + dt
+    !call KROME
+    call krome(x(:), Tgas, dt)
+    nstep = nstep + 1 
+    if (mod(nstep,10) .eq. 0) then
+      write (*,'(a,i5.1)') "nstep = ", nstep
+    endif
+    write (66,'(60(1pe15.8))') t/spy, Tgas, x(:)/ntot
+    call krome_popcool_dump(t/spy, 69)
+    if (t > 1.0d8 * spy) exit
+  enddo
+  close(66)
+
+  write (*,'(a,i5.1)') "Finished. Number of steps = ", nstep
+
+  ! ----==== Isotope decay on ====---- !
+
+  x(:) = 1d-20 !default abundances
+  ntot = 1.0d4 ! cm**-3
+  x(krome_idx_Hj) = ntot !hydrogen ion initial abundance
+
+  ! set iron abundance
+  call krome_scale_Z(x(:),0.0d0)
+  x(krome_idx_Fej) = x(krome_idx_Fe)
+  x(krome_idx_Fe) = 0.0d0
+  x(krome_idx_E) = krome_get_electrons(x(:))
+  ! set 60Fe abundance
+  call krome_set_user_tauh(1.5d6 * spy)
+  x_Fe60 =1.0d-6
+  print*, 'Fe-60 abundance =',x_Fe60
+  x(krome_idx_60Fe) = x_Fe60 * x(krome_idx_Fej)
+  ! set rate for ionisation by isotope decay
+  fe60_xi = 1.0d-10
+  print*, 'Isotope decay on.'
+  call krome_set_user_xi(fe60_xi)
+  ! set heating from isotope decay
+  call krome_set_user_Wergs(36.0d0 * krome_eV_to_erg)
+
+  Tgas = 1.0d3 !gas temperature (K)
+
+  open (66, file='idon.f.dat', status='unknown', form='formatted')
+
+  t = 0.0d0
+  dt = 1d-2 * spy ! yr -> s
+  nstep = 0
+  do
+    dt = dt * 1.1d0
+    t = t + dt
+    !call KROME
+    call krome(x(:), Tgas, dt)
+    nstep = nstep + 1 
+    if (mod(nstep,10) .eq. 0) then
+      write (*,'(a,i5.1)') "nstep = ", nstep
+    endif
+    write (66,'(60(1pe15.8))') t/spy, Tgas, x(:)/ntot
+    call krome_popcool_dump(t/spy, 70)
+    if (t > 1.0d8 * spy) exit
+  enddo
+  close(66)
+
+  write (*,'(a,i5.1)') "Finished. Number of steps = ", nstep
+
+end program test
