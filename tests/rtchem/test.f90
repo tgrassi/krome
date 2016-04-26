627b8eda028e2b9b1ed0f3d73f46a9f04e839ea9
zlaptoppeo <zlaptoppeo@zlaptop.(none)>
2016-04-27 00:53:20 +0200

    test rtchem update

--- tests/rtchem/test.f90
+++ tests/rtchem/test.f90
@@ -7,36 +7,39 @@
 program main
   use krome_main
   use krome_user
+  use krome_user_commons
   implicit none
-  integer,parameter::ngrid=30 !grid points
+  integer,parameter::ngrid=100 !grid points
   integer::i,j
   real*8::Tgas(ngrid),dt,x(krome_nmols),spy,pc,Rsun,ntot
   real*8::xall(ngrid,krome_nmols),rmin,rmax,r,t,rx(ngrid)
   real*8::Rstar,Tstar,dr,rold,op(krome_nPhotoBins)
   real*8::opx(krome_nPhotoBins),energy(krome_nPhotoBins)
-  real*8::flux(krome_nPhotoBins),Av
+  real*8::flux(krome_nPhotoBins),Av,fav
+
+  fav = 1.8d21!1d0/6.289d-22
 
   call krome_init()
 
-  Tgas(:) = 7d2 !gas temperature in K (all cells)
+  Tgas(:) = 5d1 !gas temperature in K (all cells)
+  ntot = 1d3 !total density
   spy = krome_seconds_per_year !s
   pc = 3.085d18 !1 pc in cm
   Rsun = 7d10 !sun radius in cm
-  rmin = pc*1d-3 !min radius: cm
-  rmax = pc*1d1 !box radius: cm
+  rmin = fav/ntot*1d-6 !min radius: cm
+  rmax = fav/ntot*1d1 !box radius: cm
   Rstar = 66.1*Rsun !emitting sphere radius
-  ntot = 1d4
 
   !set cosmic rays
-  call krome_set_user_crate(1.3d-17)
+  call krome_set_user_crate(5d-17)
 
   !init abundances
   xall(:,:) = 0d0
-  xall(:,krome_idx_H) = ntot
-  !xall(:,krome_idx_H2) = ntot/3.*2.
-  xall(:,krome_idx_C) = 2.5d-4*ntot
-  xall(:,krome_idx_O) = 4.7d-4*ntot
-  xall(:,krome_idx_He) = 8.5d-2*ntot
+  xall(:,krome_idx_H) = ntot/3.
+  xall(:,krome_idx_H2) = ntot/3.*2.
+  xall(:,krome_idx_C) = 1d-4*ntot !2.5d-4*ntot
+  xall(:,krome_idx_O) = 3d-4*ntot !4.7d-4*ntot
+  xall(:,krome_idx_He) = 1d-1*ntot !8.5d-2*ntot
 
   !initial time step
   dt = .1*spy
@@ -50,10 +53,10 @@ program main
   end do
 
   !uses a draine flux
-  call krome_set_photoBin_draineLog(6d0, 12d0)
-  call krome_load_opacity_table("opdust.dat",unitEnergy="micron")
+  call krome_set_photoBin_draineLin(5d0, 13.6d0)
+  call krome_load_opacity_table("opacityDraineR35.dat",unitEnergy="micron")
   !scale the draine flux by 10 times
-  call krome_photoBin_scale(1.75d4)
+  !call krome_photoBin_scale(1d1)
   call krome_photoBin_store()
   !store energy value for plot
   energy(:) = krome_get_photoBinE_mid()
@@ -79,11 +82,9 @@ program main
         r = rx(i)
         !grid spacing
         dr = r-rold
-        Av = Av + dr * sum(xall(i,:))/1.8d21
+        Av = Av + dr * sum(xall(i,:))/fav
         !opacity from photochemitry
         op(:) = op(:)*exp(-krome_get_opacity_size_d2g(x(:),Tgas(i), dr,1d-2))
-        !geometrical flux attenuation
-        opx(:) = op(:)/4d0/krome_pi!/4d0*(rstar/r)**2
         !store flux for plot
         flux(:) = krome_get_photoBinJ()
         !loop on photobins to write energy-dependent flux
@@ -92,7 +93,7 @@ program main
         end do
         write(45,*)
         !scale the flux according to opacity
-        call krome_photoBin_scale_array(opx(:))
+        call krome_photoBin_scale_array(op(:))
         !call KROME to do chemistry
         x(:) = xall(i,:)
         call krome(x(:),Tgas(i),dt)
@@ -111,7 +112,7 @@ program main
      do i=1,ngrid
         x(:) = xall(i,:)
         r = rx(i)
-        av = av + (r-rold)*sum(x) / 1.8d21
+        av = av + (r-rold)*sum(x) / fav
         write(66,'(99E17.8e3)') t/spy,av,Tgas(i),x(:)
         rold = r
      end do
@@ -127,7 +128,7 @@ program main
   do i=1,ngrid
      x(:) = xall(i,:)
      r = rx(i)
-     av = av + (r-rold)*sum(x) / 1.8d21
+     av = av + (r-rold)*sum(x) / fav
      write(55,'(99E17.8e3)') t/spy,Av,Tgas(i),x(:)/ntot
      rold = r
   end do
