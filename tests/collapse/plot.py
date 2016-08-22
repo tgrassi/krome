968f473f5855ca84140493cf4d8fcee5adcf6bcf
tgrassi <tgrassi@nbi.dk>
2016-08-22 20:51:19 +0200

    TEST: plot py collapse

---
+++ tests/collapse/plot.py
@@ -0,0 +1,23 @@
+import kp
+
+k = kp.kp()
+
+#load data
+k.loadDataFromFile("fort.22")
+
+#set xlabel
+k.xlabel = r'density/cm$^{-3}$'
+
+#plot type
+k.plog = "loglog"
+
+#plot
+k.ylabel = "fraction"
+k.multiplotLayout("21",sharex=True)
+k.multiplotAdd(columns=["ntot","H","H2"])
+
+k.ymin = 1e2
+k.ylabel = "T/K"
+k.multiplotAdd(columns=["ntot","Tgas"])
+k.multiplotShow()
+
