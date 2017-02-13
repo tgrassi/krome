8e79748eefc69c5a37f8dbb8e4987eede575d083
tgrassi <tommasograssi@gmail.com>
2017-02-13 17:26:07 +0100

    TOOLS: kexplorer html interface

--- tools/kexplorer/kexplorer_network.py
+++ tools/kexplorer/kexplorer_network.py
@@ -1,11 +1,15 @@
 import kexplorer_reaction
-import sys,subprocess,os,glob
+import sys,subprocess,os,glob,json,datetime
+
 class network:
 	reactions = dict()
 
 	minFlux = 1e-5 #minimum flux to plot
 	plotLog = True #edge thickness is log of flux
 	maxPenWidth = 5. #max edge thicknes, px
+	xvarName = "" #name of independent variable
+	xvarUnits = "" #units of independent variable
+	xvarFormat = "%e" #format of independent variable
 
 	#**********************
 	#network contructor read kexplorer file
@@ -28,15 +32,15 @@ class network:
 		arow = [x for x in srow.split(" ") if(x!="")]
 		idx = int(arow[0])
 		#parse data
-		(xvar, flux, fluxNormMax, fluxNormTot) = [float(x) for x in arow[1:5]]
+		(xvar, Tgas, flux, fluxNormMax, fluxNormTot) = [float(x) for x in arow[1:6]]
 
 		#parse verbatim reaction and create reaction object
 		if(not(idx in self.reactions)):
-			rname = (" ".join(arow[5:]))
+			rname = (" ".join(arow[6:]))
 			self.reactions[idx] = kexplorer_reaction.reaction(rname)
 
 		#add data to reaction
-		self.reactions[idx].addData(xvar, flux, fluxNormMax, fluxNormTot)
+		self.reactions[idx].addData(xvar, Tgas, flux, fluxNormMax, fluxNormTot)
 
 
 	#******************
@@ -97,9 +101,33 @@ class network:
 		self.graphMap = graphMap
 
 	#****************
+	#dump data to JSON
+	def dumpJSON(self):
+
+		jsonData = []
+		#get dependent variable data
+		xvarData = self.bestReactions[0].xvarData
+		#count data
+		numberData = len(xvarData)
+		for i in range(numberData):
+			stepData = []
+			for reaction in self.bestReactions:
+				rData = {"verbatim":reaction.verbatim, \
+					"htmlVerbatim":reaction.htmlVerbatim, \
+					"flux": reaction.fluxData[i], \
+					"fluxNormTot":reaction.fluxNormTotData[i]}
+				stepData.append(rData)
+			stepData = sorted(stepData,key=lambda x:x["flux"])[::-1]
+			jsonData.append({"data":stepData,"xvar":xvarData[i]})
+
+		with open('data.json', 'w') as outfile:
+			json.dump(jsonData, outfile)
+
+	#****************
 	#dump graph to png
 	def dumpBest(self,pngFolder="pngs/"):
 		from math import log10
+
 
 		#if png folder not present than create
 		if(not(os.path.exists(pngFolder))):
@@ -116,6 +144,9 @@ class network:
 		xvarData = self.bestReactions[0].xvarData
 		#count data
 		numberData = len(xvarData)
+
+		self.dumpJSON()
+		self.makeHTML(numberData)
 
 		#loop on data
 		for i in range(numberData):
@@ -145,12 +176,19 @@ class network:
 			#create temporary dot file
 			fout = open("tmp.dot","w")
 			fout.write("digraph G{\n")
-			fout.write("label=\""+("%e" % xvarData[i])+"\";\n")
+			xvarName = ""
+			if(self.xvarName!=""): xvarName = self.xvarName+" = "
+			xvar = xvarName+(self.xvarFormat % xvarData[i])+" "+self.xvarUnits
+			tgasVar = "Tgas = "+str(round(reaction.tgasData[i],1))+" K"
+			#add independent variable to PNG
+			fout.write("label=\""+xvar+", "+tgasVar+"\";\n")
 			#unique list of species
 			allSpecies = list(set(allSpecies))
 			#loop on species to have circle nodes
 			for species in allSpecies:
-				if(not(self.atomBase in species)): continue
+				#skip species if atomBase is present
+				if(self.atomBase!=None):
+					if(not(self.atomBase in species)): continue
 				fout.write("\""+species+"\" [shape=circle];\n")
 
 			#loop on graph map, reactants
@@ -158,6 +196,7 @@ class network:
 				#loop on graph map, products
 				for (product,maxval) in products.iteritems():
 
+					#check if reactant+prducts has atom base, if not skip
 					if(self.atomBase!=None):
 						reactantHasBase = (self.atomBase in reactant)
 						productHasBase = (self.atomBase in product)
@@ -190,7 +229,50 @@ class network:
 			myCall = "dot tmp.dot -Tpng -o "+pngName
 			subprocess.call(myCall.split(" "))
 
+	#**************
+	def loadHeader(self,fname,maxSteps):
+		fh = open(fname,"rb")
+		allFile = ""
+		for row in fh:
+			allFile += row.replace("#MAXFILE#",str(maxSteps))
+		fh.close()
+		return allFile
 
+	#**************
+	def loadFooter(self,fname):
+		fh = open(fname,"rb")
+		allFile = ""
+		for row in fh:
+			allFile += row.replace("#FOOTER_INFO#",self.getFooterInfo())
+		fh.close()
+		return allFile
+
+	#**************
+	def makeHTML(self,maxSteps):
+		fh = open("kexplorer.php","rb")
+		fout = open("index.html","w")
+		for row in fh:
+			row = row.replace("#HEADER#",self.loadHeader("header.php",maxSteps))
+			row = row.replace("#FOOTER#",self.loadFooter("footer.php"))
+			fout.write(row)
+		fh.close()
+		fout.close()
+
+	#***********************
+	def getFooterInfo(self):
+		#name of the git master file
+		masterfile = "../../.git/refs/heads/master"
+		changeset = ("x"*7) #default unknown changeset
+		#if git master file exists grep the changeset
+		if(os.path.isfile(masterfile)):
+			changeset = open(masterfile,"rb").read()
+
+		datenow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		bitbucketLink = "https://bitbucket.org/tgrassi/krome/commits/"+changeset
+		bitbucketLanding = "<a href=\"https://bitbucket.org/tgrassi/krome\" target=\"_blank\">KROME</a>"
+		hrefWiki = "<a href=\"https://bitbucket.org/tgrassi/krome/wiki/kexplorer\" target=\"_blank\">kexplorer</a>"
+		return "documentation generated with "+hrefWiki+" ("+bitbucketLanding+") - changeset: <a href=\""\
+			+ bitbucketLink + "\" target=\"_blank\">" + changeset[:7] + "</a> - " + datenow
 
 
 
