79c33a786b049a36701555877a8e9d41490db68c
tgrassi <tommasograssi@gmail.com>
2017-03-22 14:46:59 +0100

    F90: module for fit

---
+++ src/krome_fit.f90
@@ -0,0 +1,332 @@
+module krome_fit
+contains
+  !********************************************
+  subroutine init_anytab2D(filename,x,y,z,xmul,ymul)
+    use krome_commons
+    implicit none
+    character(len=*)::filename
+    character(len=60)::row_string
+    real*8::x(:),y(:),z(:,:),rout(3),xmul,ymul
+    integer::i,j,ios
+
+    !check the size of the X input array
+    if(size(x).ne.size(z,1)) then
+       print *,"ERROR: in init_anytab2D x size differs from z"
+       stop
+    end if
+
+    !check the size of the Y input array
+    if(size(y).ne.size(z,2)) then
+       print *,"ERROR: in init_anytab2D y size differs from z"
+       stop
+    end if
+
+    if (krome_mpi_rank<=1) print *,"Reading tables from "//trim(filename)
+
+    !open file and check if it exists
+    open(51,file=trim(filename),status="old",iostat=ios)
+    if(ios.ne.0) then
+       print *,"ERROR: in init_anytab2D file ",trim(filename)," not found!"
+       stop
+    end if
+
+    !skip the comments and the first line and the sizes of the data
+    ! which are already known from the pre-processing
+    do
+       read(51,'(a)') row_string
+       if(row_string(1:1)/="#") exit
+    end do
+
+    !check if first line is OK
+    if(scan(row_string,",")==0) then
+       print *,"ERROR: file "//filename//" should"
+       print *," contain the number of rows and "
+       print *," columns in the format"
+       print *,"  RR, CC"
+       print *,row_string
+       stop
+    end if
+
+    !loop to read file
+    do i=1,size(x)
+       do j=1,size(y)
+          read(51,*,iostat=ios) rout(:)
+          y(j) = rout(2)
+          z(i,j) = rout(3)
+       end do
+       x(i) = rout(1)
+       read(51,*,iostat=ios) !skip blanks
+       if(ios.ne.0) exit
+    end do
+    close(51)
+
+    xmul = 1d0/(x(2)-x(1))
+    ymul = 1d0/(y(2)-y(1))
+
+  end subroutine init_anytab2D
+
+  !******************************
+  subroutine test_anytab2D(fname,x,y,z,xmul,ymul)
+    implicit none
+    integer::i,j
+    real*8::x(:),y(:),z(:,:),xmul,ymul,xx,yy,zz
+    character(len=*)::fname
+
+    open(91,file=fname//".fit",status="replace")
+    open(92,file=fname//".org",status="replace")
+    do i=1,size(x)
+       do j=1,size(y)
+          xx = x(i)
+          yy = y(j)
+          zz = fit_anytab2D(x(:),y(:),z(:,:),xmul,ymul,xx,yy)
+          write(91,*) xx,yy,zz
+          write(92,*) x(i),y(j),z(i,j)
+       end do
+       write(91,*)
+       write(92,*)
+    end do
+    print *,"original file wrote in ",fname//".org"
+    print *,"fit test file wrote in ",fname//".fit"
+
+  end subroutine test_anytab2D
+
+  !******************************
+  function fit_anytab2D(x,y,z,xmul,ymul,xx,yy)
+    real*8::fit_anytab2D,x(:),y(:),z(:,:),xmul,ymul,xx,yy
+    real*8::zleft(size(x)),zright(size(x)),zl,zr
+    integer::ipos,i1,i2
+
+    ipos = (yy-y(1)) * ymul + 1
+    i1 = min(max(ipos,1),size(y)-1)
+    i2 = i1 + 1
+    zleft(:) = z(:,i1)
+    zright(:) = z(:,i2)
+
+    zl = fit_anytab1D(x(:),zleft(:),xmul,xx)
+    zr = fit_anytab1D(x(:),zright(:),xmul,xx)
+
+    fit_anytab2D = (yy-y(i1))*ymul*(zr-zl)+zl
+
+  end function fit_anytab2D
+
+  !*********************
+  function fit_anytab1D(x,z,xmul,xx)
+    real*8::fit_anytab1D,x(:),z(:),xmul,xx,p
+    integer::ipos,i1,i2
+
+    ipos = (xx-x(1)) * xmul + 1
+    i1 = min(max(ipos,1),size(x)-1)
+    i2 = i1 + 1
+
+    p = (xx-x(i1)) * xmul
+
+    fit_anytab1D = p * (z(i2) - z(i1)) + z(i1)
+
+  end function fit_anytab1D
+
+  function fit_anytab2D_linlog(x,y,z,xmul,ymul,xx,yy)
+    real*8::fit_anytab2D_linlog,x(:),y(:),z(:,:),xmul,ymul,xx,yy
+    real*8::zleft(size(x)),zright(size(x)),zl,zr
+    integer::ipos,i1,i2
+
+    ipos = (yy-y(1)) * ymul + 1
+    i1 = min(max(ipos,1),size(y)-1)
+    i2 = i1 + 1
+    zleft(:) = z(:,i1)
+    zright(:) = z(:,i2)
+
+    zl = fit_anytab1D_linlog(x(:),zleft(:),xmul,xx)
+    zr = fit_anytab1D_linlog(x(:),zright(:),xmul,xx)
+
+    fit_anytab2D_linlog = (yy-y(i1))*ymul*(zr-zl)+zl
+
+  end function fit_anytab2D_linlog
+
+  !*********************
+  function fit_anytab1D_linlog(x,z,xmul,xx)
+    real*8::fit_anytab1D_linlog,x(:),z(:),xmul,xx,p,z2,z1
+    integer::ipos,i1,i2
+
+    ipos = (xx-x(1)) * xmul + 1
+    i1 = min(max(ipos,1),size(x)-1)
+    i2 = i1 + 1
+
+    p = (xx-x(i1)) * xmul
+
+    z2 = z(i2)
+    z1 = z(i1)
+    if(z1<0d0 .and. z2<0d0) then
+       z1 = log10(-z1)
+       z2 = log10(-z2)
+       fit_anytab1D_linlog = -1d1**(p * (z2 - z1) + z1)
+       return
+    end if
+
+    if(z1>0d0 .and. z2>0d0) then
+       z1 = log10(z1)
+       z2 = log10(z2)
+       fit_anytab1D_linlog = 1d1**(p * (z2 - z1) + z1)
+       return
+    end if
+
+    fit_anytab1D_linlog = (p * (z2 - z1) + z1)
+
+  end function fit_anytab1D_linlog
+
+
+  !*****************************
+  !spline interpolation at t using array  x,y (size n) as data
+  function fspline(x,y,t)
+    implicit none
+    real*8::fspline,x(:),y(:),b(size(x)),c(size(x)),d(size(x)),t
+    integer::n
+
+    n = size(x)
+    call spline(x(:),y(:),b(:),c(:),d(:),n)
+    fspline = ispline(t,x(:),y(:),b(:),c(:),d(:),n)
+
+  end function fspline
+
+  !*******************************+
+  subroutine spline(x, y, b, c, d, n)
+    !======================================================================
+    !  Calculate the coefficients b(i), c(i), and d(i), i=1,2,...,n
+    !  for cubic spline interpolation
+    !  s(x) = y(i) + b(i)*(x-x(i)) + c(i)*(x-x(i))**2 + d(i)*(x-x(i))**3
+    !  for  x(i) <= x <= x(i+1)
+    !  Alexadner L Godunov (ODU): January 2010
+    !
+    !  http://ww2.odu.edu/~agodunov/computing/programs/book2/Ch01/spline.f90
+    !----------------------------------------------------------------------
+    !  input..
+    !  x = the arrays of data abscissas (in strictly increasing order)
+    !  y = the arrays of data ordinates
+    !  n = size of the arrays xi() and yi() (n>=2)
+    !  output..
+    !  b, c, d  = arrays of spline coefficients
+    !  comments ...
+    !  spline.f90 program is based on fortran version of program spline.f
+    !  the accompanying function fspline can be used for interpolation
+    !======================================================================
+    implicit none
+    integer::n
+    real*8::x(n), y(n), b(n), c(n), d(n)
+    integer::i, j, gap
+    real*8::h
+
+    gap = n-1
+
+    !check input
+    if(n<2) return
+    if(n<3) then
+       b(1) = (y(2)-y(1))/(x(2)-x(1)) !linear interpolation
+       c(1) = 0d0
+       d(1) = 0d0
+       b(2) = b(1)
+       c(2) = 0d0
+       d(2) = 0d0
+       return
+    end if
+
+    !step 1: preparation
+    d(1) = x(2) - x(1)
+    c(2) = (y(2) - y(1))/d(1)
+    do i = 2, gap
+       d(i) = x(i+1) - x(i)
+       b(i) = 2d0*(d(i-1) + d(i))
+       c(i+1) = (y(i+1) - y(i))/d(i)
+       c(i) = c(i+1) - c(i)
+    end do
+
+    ! step 2: end conditions
+    b(1) = -d(1)
+    b(n) = -d(n-1)
+    c(1) = 0d0
+    c(n) = 0d0
+    if(n.ne.3) then
+       c(1) = c(3)/(x(4)-x(2)) - c(2)/(x(3)-x(1))
+       c(n) = c(n-1)/(x(n)-x(n-2)) - c(n-2)/(x(n-1)-x(n-3))
+       c(1) = c(1)*d(1)**2/(x(4)-x(1))
+       c(n) = -c(n)*d(n-1)**2/(x(n)-x(n-3))
+    end if
+
+    ! step 3: forward elimination
+    do i = 2, n
+       h = d(i-1)/b(i-1)
+       b(i) = b(i) - h*d(i-1)
+       c(i) = c(i) - h*c(i-1)
+    end do
+
+    ! step 4: back substitution
+    c(n) = c(n)/b(n)
+    do j = 1, gap
+       i = n-j
+       c(i) = (c(i) - d(i)*c(i+1))/b(i)
+    end do
+
+    ! step 5: compute spline coefficients
+    b(n) = (y(n) - y(gap))/d(gap) + d(gap)*(c(gap) + 2d0*c(n))
+    do i = 1, gap
+       b(i) = (y(i+1) - y(i))/d(i) - d(i)*(c(i+1) + 2d0*c(i))
+       d(i) = (c(i+1) - c(i))/d(i)
+       c(i) = 3d0*c(i)
+    end do
+    c(n) = 3d0*c(n)
+    d(n) = d(n-1)
+  end subroutine spline
+
+  !*******************************
+  function ispline(u, x, y, b, c, d, n)
+    !======================================================================
+    ! function ispline evaluates the cubic spline interpolation at point z
+    ! ispline = y(i)+b(i)*(u-x(i))+c(i)*(u-x(i))**2+d(i)*(u-x(i))**3
+    ! where  x(i) <= u <= x(i+1)
+    !  Alexadner L Godunov (ODU): January 2010
+    !
+    !  http://ww2.odu.edu/~agodunov/computing/programs/book2/Ch01/spline.f90
+    !----------------------------------------------------------------------
+    ! input..
+    ! u       = the abscissa at which the spline is to be evaluated
+    ! x, y    = the arrays of given data points
+    ! b, c, d = arrays of spline coefficients computed by spline
+    ! n       = the number of data points
+    ! output:
+    ! ispline = interpolated value at point u
+    !=======================================================================
+    implicit none
+    real*8::ispline
+    integer::n
+    real*8::u, x(n), y(n), b(n), c(n), d(n)
+    integer::i, j, k
+    real*8::dx
+
+    ! if u is ouside the x() interval take a boundary value (left or right)
+    if(u<=x(1)) then
+       ispline = y(1)
+       return
+    end if
+
+    if(u>=x(n)) then
+       ispline = y(n)
+       return
+    end if
+
+    ! binary search for for i, such that x(i) <= u <= x(i+1)
+    i = 1
+    j = n+1
+    do while (j>i+1)
+       k = (i+j)/2
+       if(u<x(k)) then
+          j=k
+       else
+          i=k
+       end if
+    end do
+
+    ! evaluate spline interpolation
+    dx = u - x(i)
+    ispline = y(i) + dx*(b(i) + dx*(c(i) + dx*d(i)))
+
+  end function ispline
+
+end module krome_fit
