7be21f3ee4941e338b4b93c1350d18515c180495
Jels Boulangier <boulangier.jels@gmail.com>
2017-04-20 16:15:00 +0200

    Abundance: colorplot,evolution. Create most fluxy network

--- tools/kexplorer/main.py
+++ tools/kexplorer/main.py
@@ -3,20 +3,40 @@
 # of png files to show the system evolution while evolving
 import kexplorer_network
 
-fileName = "demo.dat"
+fileName = "Fluxes_demoJB.dat"
+fileNameEvolution = "Grid_demoJB.dat"
+oldNetwork = "react_primordial3"
+newNetwork = "react_primordial3_reduced"
+
+TgasInterest = [1e3,5e3,1e4]
+xvarInterest = [1e-11,1e-13]
+elemInterest = ["H","H2"]
 
 #create network from explorer file
-network = kexplorer_network.network(fileName)
+network = kexplorer_network.network(fileName,fileNameEvolution)
 
-network.xvarName = "density"
-network.xvarUnits = "cm-3"
+network.xvarName = "Density"
+network.xvarUnits = "g/cm^3"
 
 #find best matching criteria
-network.findBest()
+network.findBest(threshold=1e-2)
 
 #print most fluxy reactions
 network.listBest()
 
 #dump to png folder
-network.dumpBest()
+network.dumpBest("pngs/")
 
+#all below added by Jels Boulangier 30/03/2017
+#create new network file for most fluxy reactions
+network.networkBest(oldNetwork,newNetwork)
+
+#make abundace colormaps for model grid
+#network.abundanceColormapAll(elemInt=elemInterest,pngFolder=pngsOut)
+#if elemInt not specified, this will do all elements
+network.abundanceColormapAll(pngFolder="pngs/")
+
+
+#make time evolution plot for interesting model grid points
+network.abundanceEvolution(atom="H2",tgasInt=TgasInterest,xvarInt=xvarInterest)
+network.abundanceEvolution(atom="H",tgasInt=TgasInterest,xvarInt=xvarInterest)
