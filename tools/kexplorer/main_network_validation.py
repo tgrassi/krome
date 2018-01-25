3540ab4131dad2aefa4274645118b56a2af06e6c
JelsB <boulangier.jels@gmail.com>
2018-01-25 15:19:59 +0000

    KEXPLORER: Tool to quantitatively validate reduced networks.

---
+++ tools/kexplorer/main_network_validation.py
@@ -0,0 +1,62 @@
+# This tool is used to validate reduced networks.
+# It can make absolute abundance color plots in a grid space
+# for a reduced and non-reduced network. This can directly be used
+# to see if both network produce the same results.
+# I can also make a realtive difference abundance plot, for a more
+# quantative comparison.
+# It can also make abundance evolution plots for each species
+# in a choosen grid space.
+# These functions load data from a file that contains the abundances
+# of all species at multiple time steps in a choosen grid space.
+# Example format: Grid_demo.dat
+import kexplorer_network
+import kexplorer_compareNetworks
+
+
+fileName = "Fluxes_demo.dat"
+# "fileNameEvolution" is a .dat files with abundances of species in temperature
+# -density grid. One for the original network and one for the reduced network.
+# Example file = Grid_demo.dat
+fileNameEvolutionFull =
+fileNameEvolutionReduced =
+# When you want to plot the abundance evolution of certain species
+# at choosen values of temperature and e.g. density
+TgasInterest = [1e3,5e3,1e4] # in K
+xvarInterest = [1e-11,1e-13] # in g/cm^3
+# When you want to plot abundance color maps for a certain set of species
+# in a grid of temperature and e.g. density using "fileNameEvolution"
+elemInterest = ["H","H2"]
+
+#create network objects for full and reduced from explorer file
+network_reduced = kexplorer_network.network(fileName,fileNameEvolutionReduced)
+network_full = kexplorer_network.network(fileName,fileNameEvolutionFull)
+
+# used variable besides temperature
+network_reduced.xvarName = "Density"
+network_reduced.xvarUnits = "g/cm^3"
+network_full.xvarName = "Density"
+network_full.xvarUnits = "g/cm^3"
+
+
+#### Validation of new reduced network.
+
+# Make abundace colormaps for model grid for species of interest
+network_full.abundanceColormapAll(elemInt=elemInterest,pngFolder="tpngsFull/")
+network_reduced.abundanceColormapAll(elemInt=elemInterest,pngFolder="tpngsRed/")
+#if elemInt not specified, this will do all species
+#network.abundanceColormapAll(pngFolder="pngs/")
+
+# Make relative abundace colormaps between full and reduced network
+kexplorer_compareNetworks.compareAbundances(network_full, network_reduced,
+                                            pngFolder="tpngsCompare/")
+
+
+#make time evolution plot for interesting model grid points
+network_full.abundanceEvolution(atom="H2", tgasInt=TgasInterest,
+                                xvarInt=xvarInterest, pngFolder="pngsFull/")
+network_full.abundanceEvolution(atom="H", tgasInt=TgasInterest,
+                                xvarInt=xvarInterest, pngFolder="pngsFull/")
+network_red.abundanceEvolution(atom="H2", tgasInt=TgasInterest,
+                                xvarInt=xvarInterest, pngFolder="pngsRed/")
+network_red.abundanceEvolution(atom="H", tgasInt=TgasInterest,
+                                xvarInt=xvarInterest, pngFolder="pngsRed/")
