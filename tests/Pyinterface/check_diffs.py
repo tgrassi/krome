3b5678547b902aa0ec7f8e1c4b97ce6f79e6e3e6
tgrassi <tommasograssi@gmail.com>
2019-04-12 17:13:11 +0200

    TEST: pytest cfr fixed

--- tests/Pyinterface/check_diffs.py
+++ tests/Pyinterface/check_diffs.py
@@ -1,28 +1,32 @@
-from filecmp import cmp
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    path = './'
-    prefix = ['idoff.','idon.']
-    suffix = '.dat'
-    compare = ['f','py']
+files = [["idoff.f.dat", "idoff.py.dat"],
+         ["idon.f.dat", "idon.py.dat"]]
 
-    for p in prefix:
-        file0, file1 = [path+p+c+suffix for c in compare]
-        result = cmp(file0, file1)
-        if not result:
-            print ': {0}, {1} DO NOT match!!'.format(file0.strip('./'), file1.strip('./'))
-        else:
-            print ': {0}, {1} are the same'.format(file0.strip('./'), file1.strip('./'))
+colors =  ['#1F77B4', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#CFECF9', '#7F7F7F', '#BCBD22', '#17BECF']
+markers = [None, "x"]
+linewidths = [1, 0]
 
-    path = './'
-    prefix = 'fort.'
-    suffix = ''
-    compare = [('69','71'), ('70','72')]
+for fnames in files:
+    plt.clf()
+    for ii, fname in enumerate(fnames):
+        data = np.loadtxt(fname).T
+        for jj, dd in enumerate(data[2:]):
+            label = ""
+            if jj == 0:
+                label = fname
+            plt.loglog(data[0],
+                       dd,
+                       color=colors[jj],
+                       marker=markers[ii],
+                       markersize=5.,
+                       linewidth=linewidths[ii],
+                       label=label)
 
-    for c in compare:
-        file0, file1 = [path+prefix+cc for cc in c]
-        result = cmp(file0, file1)
-        if not result:
-            print '| {0}, {1} DO NOT match!!'.format(file0.strip('./'), file1.strip('./'))
-        else:
-            print '| {0}, {1} are the same'.format(file0.strip('./'), file1.strip('./'))
+    plt.ylim(1e-8, 3e0)
+    plt.ylabel("abundances / cm-3")
+    plt.xlabel("time / yr")
+    plt.legend(loc="best")
+    plt.show()
+
