74f8daf973d6fa54ea8aa66dbe12dd324fa9e9c8
tgrassi <tgrass@mep.mpg.de>
2024-07-22 10:16:54 +0200

    typo fix, thx P.Sharda

--- src/krome_heating.f90
+++ src/krome_heating.f90
@@ -86,9 +86,9 @@ contains
 #IFKROME_useHeatingZExtended
     !this parameter controls the smoothness of the
     ! merge between the two cooling / heating functions
-    smooth = 1.d-3
+    smooth = 1d-3
     !smoothing functions | f1+f2=1 | f1 is not needed
-    f2 = = (tanh(smooth*(Tgas-1d4))+1.d0)*0.5d0 
+    f2 = (tanh(smooth*(Tgas-1d4))+1d0)*0.5d0
 #ENDIFKROME
 
 #IFKROME_useHeatingZCIE
